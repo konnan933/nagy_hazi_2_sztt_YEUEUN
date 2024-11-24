391a40b51845e489f9c6fcdc85798cdd7cc81aea
konnan933 <95681490+konnan933@users.noreply.github.com>
2024-11-24 12:10:20 +0100

    book class

---
+++ Libary/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libary.Models
+{
+    public class Book
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public List<string> Authors { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Id}: {Title} ({Genre}), Authors: {string.Join(", ", Authors)}";
+        }
+    }
+}
